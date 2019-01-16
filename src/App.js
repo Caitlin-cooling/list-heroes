4899b10ea11564e620ad39e0a969d51e96410287
Caitlin Cooling <ccooling97@gmail.com>
2019-01-16 19:19:03 +0000

    Images
    - Adding Marvel logo
    - Creating an images component to handle the images

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
-import logo from './DC_Comics_logo.png'
+import dc from './DC_Comics_logo.png'
+import marvel from './marvel-logo.png'
 import './App.css';
+
+function Images() {
+  return (
+    <div>
+      <img src={dc} alt="DC logo"/>
+      <img src={marvel} alt="Marvel logo"/>
+    </div>
+  )
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h2>Welcome to the heroes app</h2>
-        <img src={logo} alt="DC logo"/>
+        <Images/>
       </div>
     );
   }
