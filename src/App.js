b60d6a0eb9b3a03d29cc5d0da8e82ce2f708075a
Caitlin Cooling <ccooling97@gmail.com>
2019-01-16 19:04:42 +0000

    Begin to style: - Add logo - Center text

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import logo from './DC_Comics_logo.png'
 import './App.css';
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <h2>Welcome to the heroes app</h2>
+        <img src={logo} alt="DC logo"/>
       </div>
     );
   }
